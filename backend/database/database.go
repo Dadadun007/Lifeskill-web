48767b9391dcc8ece6c85bd5ea9d012282b86092
Nitipoom Phramsrichai <nitipoom.phra@kmutt.ac.th>
2025-03-20 18:01:48 +0700

    conect database

---
+++ backend/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"  
+	port     = 5432         
+	user     = "postgres"     
+	password = "12345" 
+	dbname   = "lifeskill" 
+)
+
+var DB *sql.DB
+
+func ConnectDatabase() {
+	// Connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	var err error
+	DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully Database connected!")
+}
--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	// "github.com/dadadun/lifskill/user"
+	"github.com/dadadun/lifskill/database"
+	"fmt"
 )
 
 func main() {
 	app := fiber.New()
+	fmt.Println("Starting application...")
+
+	database.ConnectDatabase()
+
+	fmt.Println("Application started successfully!")
   
 	app.Get("/hello", func(c *fiber.Ctx) error {
 	  return c.SendString("Hello World")
