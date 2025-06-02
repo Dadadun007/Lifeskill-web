a8bb9a2fc38fb8edaf8b5cff1a3e2a6202b035f9
Nitipoom Phramsrichai <nitipoom.phra@kmutt.ac.th>
2025-06-03 00:38:06 +0700

    Revert "re version"

--- frontend/lifskill_frontend/vite.config.js
+++ frontend/lifskill_frontend/vite.config.js
@@ -4,22 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
-  base: '/',
 })
